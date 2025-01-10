8b31cdbcc84613c86bc438b6d45919d21ee2ae92
罗博 <14852764+luoboxiangqianchong@user.noreply.gitee.cominit>
2025-01-10 15:49:39 +0800

    把框架加入admincontainer.vue里，在路由里配置调用框架

--- front/src/main.js
+++ front/src/main.js
@@ -4,10 +4,16 @@ import router from './router';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import axios from 'axios';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+
+
 
 const app=createApp(App);
 app.use(ElementPlus);
 app.use(router);
-app.mount('#app');
 axios.defaults.baseURL='localhost:8080'
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component);
+}
 
+app.mount('#app');
